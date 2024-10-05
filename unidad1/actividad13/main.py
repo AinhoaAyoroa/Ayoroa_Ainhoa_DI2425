55758f4fbd73bbc3460fc53bcd53a2a70b71aa4d
AinhoaAyoroa <ainayorub@alu.edu.gva.es>
2024-10-06 00:20:49 +0200

    update

---
+++ unidad1/actividad13/main.py
@@ -0,0 +1,10 @@
+
+try:
+    fichero = open("~/AinhoaAyoroa/desarrollo_interfaces/operaciones.txt")
+except Exception:
+    print("Error al leer el fichero: ", Exception)
+else:
+    try:
+        
+    except Exception:
+        print("")
