6c276884602a0d2ee9cea2db73ed8568e73ec149
AinhoaAyoroa <ainayorub@alu.edu.gva.es>
2024-10-07 10:20:13 +0200

    update ejercicios 12 y 14

--- unidad1/actividad12/main.py
+++ unidad1/actividad12/main.py
@@ -5,8 +5,6 @@ numero_user = None
 print("--- J U E G O  D E  A D I V I N A R --- \n ")
 while (numero_user != numero_random):
     
-    
-    
     try:
         numero_user = int(input("Introduce un numero: "))
         assert numero_user == numero_random
